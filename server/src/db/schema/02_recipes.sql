fb1144443dac0de349923aad005b5aba1336971f
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-03 15:25:27 -0500

    minor correction

--- server/src/db/schema/02_recipes.sql
+++ server/src/db/schema/02_recipes.sql
@@ -7,7 +7,7 @@ CREATE TABLE recipes (
   prep_time INTEGER,
   cook_time INTEGER,
   servings INTEGER,
-  descriptiom VARCHAR (255),
+  description VARCHAR (255),
   ingredients VARCHAR(255),
   cooking_instructions VARCHAR(255),
   video_link VARCHAR(255)
