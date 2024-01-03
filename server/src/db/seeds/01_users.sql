ce6dcab046b852b3ccbb360fda4890ef406fd8a7
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-03 16:22:13 -0500

    add second user

--- server/src/db/seeds/01_users.sql
+++ server/src/db/seeds/01_users.sql
@@ -4,4 +4,8 @@ VALUES
     (
         'adm@gmail.com',
         '$2a$12$NoYzX814w6KIOznDnktvYeengks8.LYdia/6bL6UmBvoJSWxX.qj.'
+    )
+    (
+        'test@test.com',
+        '$2a$12$NoYzX814w6KIOznDnktvYeengks8.LYdia/6bL6UmBvoJSWxX.qj.'
     );
