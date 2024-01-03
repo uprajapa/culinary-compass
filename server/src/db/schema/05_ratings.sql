70746524ade9d75f50ff7e12c725440b395554d9
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-03 14:21:30 -0500

    add check to verify that rating is between 0 and 5

--- server/src/db/schema/05_ratings.sql
+++ server/src/db/schema/05_ratings.sql
@@ -4,5 +4,5 @@ CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
   recipe_id INTEGER REFERENCES recipes(id) NOT NULL,
-  rating INTEGER NOT NULL
+  rating INTEGER NOT NULL CHECK (rating between 0 and 5)
 );
