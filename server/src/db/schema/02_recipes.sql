00d78caca12e321c51cf6d16e5b7348eb10dc3ba
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-03 14:51:06 -0500

    create recipe table?

---
+++ server/src/db/schema/02_recipes.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS recipess;
+
+CREATE TABLE recipes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  prep_time INTEGER,
+  cook_time INTEGER,
+  servings INTEGER,
+  descriptiom VARCHAR (255),
+  ingredients VARCHAR(255),
+  cooking_instructions VARCHAR(255),
+  video_link VARCHAR(255)
+);
