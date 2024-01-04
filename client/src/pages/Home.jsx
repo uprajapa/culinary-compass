10315c33b7cda710da5fedc84b8805d1793397eb
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-04 12:46:17 -0500

    initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 function Home({ users, message }) {
   return (
     <div className="m-8">
-      {message ? (
+      <h1>Recipes Carousel</h1>
+      <h2>list two recipes</h2>
+      {/* {message ? (
         <p>{message}</p>
       ) : (
         <ul>
           {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
         </ul>
-      )}
+      )} */}
     </div>
   );
 }
