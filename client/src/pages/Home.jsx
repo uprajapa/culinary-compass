ecba48016176b1222c3c3d32435537baa5ec7d10
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-08 11:23:42 -0500

    move code for carousel to new FoodCarousel component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,13 @@
-import Carousel from 'react-bootstrap/Carousel';
+import react from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FoodCarousel from "../components/FoodCarousel"
 import "./Home.css"
 function Home({ users, message }) {
   return (
     <div className="home">
-      <div className="carousel">
-        <Carousel>
-        <Carousel.Item controls={true}>
-          <img src="https://www.closetcooking.com/wp-content/uploads/2008/08/Corn-and-Black-Bean-Quesadillas-500.jpg" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item controls={true}>
-          <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item controls={true}>
-          <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      </div>
+
+        <FoodCarousel />
+      
       <div className="recipe-list-item">
         <div className="profile-pic">
           <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
