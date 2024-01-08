96d3915c1ff2258c5008b3a2437a9fc98b6dee84
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-08 06:57:02 -0500

    change classNames and add recipe-title className

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Home.css"
 function Home({ users, message }) {
   return (
-    <div className="Home">
-      <div className="Carousel">
+    <div className="home">
+      <div className="carousel">
         <Carousel>
         <Carousel.Item controls={true}>
           <img src="https://www.closetcooking.com/wp-content/uploads/2008/08/Corn-and-Black-Bean-Quesadillas-500.jpg" />
@@ -31,17 +31,21 @@ function Home({ users, message }) {
         </Carousel.Item>
       </Carousel>
       </div>
-      <div className="RecipeListItem">
-        <div className="ProfilePic">
+      <div className="recipe-list-item">
+        <div className="profile-pic">
           <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
         </div>
-        <div className="ProfileInfo">
-          <h1>Recipe Name:</h1>
-          <h3>Chef Name:</h3>
+        <div className="profile-info">
+          <div className="recipe-title">
+            <p>Recipe Name by Chef Name</p>
+          </div>
           <p>Time:</p>
           <p>Est Budget:</p>
           <p>Cuisine:</p>
           <p>Description:</p>
+        </div>
+        <div>
+
         </div>
       </div>
 
