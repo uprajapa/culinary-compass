b976e88e9d032da57aa212e270cdb3860ebc02c3
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-08 09:54:59 -0500

    add second recipe list item for testing purposes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -48,6 +48,23 @@ function Home({ users, message }) {
 
         </div>
       </div>
+      <div className="recipe-list-item">
+        <div className="profile-pic">
+          <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
+        </div>
+        <div className="profile-info">
+          <div className="recipe-title">
+            <p>Recipe Name by Chef Name</p>
+          </div>
+          <p>Time:</p>
+          <p>Est Budget:</p>
+          <p>Cuisine:</p>
+          <p>Description:</p>
+        </div>
+        <div>
+
+        </div>
+      </div>
 
         {/* {message ? (
           <p>{message}</p>
