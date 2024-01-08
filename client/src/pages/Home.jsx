a76b822153247ce2c171c7a2d6daa7732338cc14
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-08 11:35:14 -0500

    transfer for recipe item to new RecipeItem component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,15 @@
 import react from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FoodCarousel from "../components/FoodCarousel"
+import RecipeItem from "../components/RecipeItem"
 import "./Home.css"
 function Home({ users, message }) {
   return (
     <div className="home">
 
         <FoodCarousel />
-      
-      <div className="recipe-list-item">
-        <div className="profile-pic">
-          <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
-        </div>
-        <div className="profile-info">
-          <div className="recipe-title">
-            <p>Recipe Name by Chef Name</p>
-          </div>
-          <p>Time:</p>
-          <p>Est Budget:</p>
-          <p>Cuisine:</p>
-          <p>Description:</p>
-        </div>
-        <div>
-
-        </div>
-      </div>
-      <div className="recipe-list-item">
-        <div className="profile-pic">
-          <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
-        </div>
-        <div className="profile-info">
-          <div className="recipe-title">
-            <p>Recipe Name by Chef Name</p>
-          </div>
-          <p>Time:</p>
-          <p>Est Budget:</p>
-          <p>Cuisine:</p>
-          <p>Description:</p>
-        </div>
-        <div>
-
-        </div>
-      </div>
+        <RecipeItem />
+        <RecipeItem />
 
         {/* {message ? (
           <p>{message}</p>
