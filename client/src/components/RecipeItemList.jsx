be3e1730cbf914f73b1fd66b6ddb2f9a50c5ede7
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-09 16:05:19 -0500

    add map function to create array of recipe items

--- client/src/components/RecipeItemList.jsx
+++ client/src/components/RecipeItemList.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import RecipeItem from './RecipeItem';
 
-const RecipeItemList = () => {
+const RecipeItemList = (props) => {
 
+  const { topRecipes } = props;
+  console.log(topRecipes)
+  const recipeListArray = topRecipes.map((recipe)=>
+    <RecipeItem 
+    key={recipe.id}
+    recipe_name={recipe.recipe_name}
+    chef_name={recipe.chef_name}
+    time={recipe.time}
+    cuisine={recipe.cuisine}
+    description={recipe.description}
+    />
+  );
   return(
-    <RecipeItem />
+    <ul>
+      {recipeListArray}
+    </ul>
   );
 };
 
