9876a6081627918aaa9efc56ac7aa6590d81f8de
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-10 15:40:23 -0500

    implement useEffect and axios get

--- client/src/hooks/useTopThreeRecipes.js
+++ client/src/hooks/useTopThreeRecipes.js
@@ -1,2 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
+const useTopThreeRecipes = () => {
+  const [topThreeRecipes, setTopThreeRecipes] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/topthreerecipes")
+      .then((response) => setTopThreeRecipes(response.data.recipes))
+      .catch((err) => console.error("Error fetching recipes", err))
+  }, []);
+
+  return { topThreeRecipes }
+};
+
+export default useTopThreeRecipes;
