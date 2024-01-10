ecacc170b264a3610d7a77c0274a80aa619c90b1
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-10 08:53:05 -0500

    fixes after merging in main branch

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -13,19 +13,4 @@ router.get("/", (req, res) => {
     });
 });
 
-module.exports = router;const express = require("express");
-const router = express.Router();
-const recipesQueries = require("../db/queries/recipes");
-
-router.get("/", (req, res) => {
-  recipesQueries
-    .findAll()
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
 module.exports = router;
