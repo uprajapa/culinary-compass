70fe4099a60b6c8f50331e392b4a23275ff42c55
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-10 15:27:50 -0500

    build backend route for topthreerecipes

--- server/src/db/queries/recipes.js
+++ server/src/db/queries/recipes.js
@@ -29,5 +29,19 @@ const findTopRatedRecipes = async () => {
     return { success: false, message: "Authentication failed" };
   }
 };
+const findTopThreeRecipes = async () => {
+  try {
+    const query = "SELECT recipes.id AS recipes_id, recipes.chef_name, recipes.recipe_name, recipes.cook_time, recipes.cuisine, recipes.description FROM ratings JOIN recipes ON recipes.id = ratings.recipe_id WHERE rating > 4 GROUP BY recipe_id, recipes.id, recipes.video_link LIMIT 3;";
+    const result = await db.query(query);
+    if (result.rowCount > 0) {
+      console.log(result.rows);
+      return { success: true, recipes: result.rows };
+    } else {
+      return { success: true, recipes: [] };
+    }
+  } catch (error) {
+    return { success: false, message: "Authentication failed" };
+  }
+};
 
-module.exports = { findAll, findTopRatedRecipes };
+module.exports = { findAll, findTopRatedRecipes, findTopThreeRecipes };
