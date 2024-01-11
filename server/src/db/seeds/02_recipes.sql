2fe37db817611c2b0421325e57f450f48111a584
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-11 14:43:12 -0500

    add photo_link column and reseed

--- server/src/db/seeds/02_recipes.sql
+++ server/src/db/seeds/02_recipes.sql
@@ -10,7 +10,8 @@ INSERT INTO
                 description,
                 ingredients,
                 cooking_instructions,
-                video_link
+                video_link,
+                photo_link
         )
 VALUES
         (
@@ -24,6 +25,7 @@ VALUES
                 'These Hearty Black Bean Quesadillas are an easy vegetarian snack or light meal that are filling, flavorful, and freezer-friendly!',
                 '1 15oz.can black beans, 1 cup frozen corn, 1/2 cup red onion, 1 clove garlic, 1/4 bunch fresh cilantro (about ½ cup chopped), 2 cups shredded cheddar cheese, 1 batch taco seasoning, 10 flour tortillas (7-inch diameter)',
                 '1. Drain the black beans and add them to a bowl along with the frozen corn (no need to thaw), 2. Finely dice the onion, mince the garlic, and roughly chop the cilantro., 3. Add the onion, garlic, cilantro, shredded cheddar, and taco seasoning to the bowl with the beans and corn. Stir until everything is evenly combined and coated in seasoning., 4. Place a half cup of the filling on one side of each tortilla and fold over. Cook the quesadillas in a skillet over medium heat on each side until brown and crispy and the cheesy filling has melted. Slice into triangles then serve.',
+                'https://www.youtube.com/watch?v=qH__o17xHls',
                 'https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg'
         ),
         (
@@ -37,7 +39,8 @@ VALUES
                 'Quick and easy delicious egg fried rice',
                 '3 tbsp cooking oil, 4 eggs, beaten with pinch of salt, 4 cups cold jasmine rice, 2 tbsp soy sauce, pinch of salt, 4 sliced green onions',
                 'Instructions in video',
-                'https://www.youtube.com/watch?v=qH__o17xHls'
+                'https://www.youtube.com/watch?v=qH__o17xHls',
+                'https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg'
         ),
         (
                 01,
@@ -50,7 +53,8 @@ VALUES
                 'super quick Indian-inspired dish',
                 '2lbs/1kg boneless skinless chicken thighs, salt, Neutral oil (i’m using light olive oil), 1 medium white onion, cut into chunks, 2-3”/5-7cm ginger, cut into coins, 5-7 garlic cloves, 15g or 2.5Tbsp curry powder, 5g or 2tsp chili powder, 5g or 1 3/4tsp cumin, 5g or 1 ¾ tsp turmeric, 15g or 2.5Tbsp garam masala, 1-2g or 1-1.5tsp  cayenne, 25g or 1.5Tbsp tomato paste, 300g or 1 1/3c water, 400g or 14oz tomato puree (½ of a 28oz can), Sugar, XL handful baby spinach (1 bag), 50-75g or 1/2c frozen peas, 30-40g or ⅛-1/4c  heavy cream',
                 'Instructions in Video',
-                'https://www.youtube.com/watch?v=ZYzBiHYOULs'
+                'https://www.youtube.com/watch?v=ZYzBiHYOULs',
+                'https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg'
         ),
         (
                 01,
@@ -63,7 +67,8 @@ VALUES
                 'Learn how to make the best guacamole! This recipe is so easy to make, only 6 ingredients, and few minutes of work and have fresh, creamy and delicious authentic guacamole. Serve it as a dip with some tortilla chips.',
                 '3 medium avocados, ripe, 1 Tomato, cored and chopped (optional), 1 green chili or jalapeño, finely chopped, 1/2 small red/while onion, finely chopped, 2-3 tablespoons Cilantro, finely chopped, 1 small Lemon/lime, juiced, Salt to taste',
                 '1. Slice the avocados in half, remove the pit and skin and place in a mixing bowl., 2. Add lime/lemon juice, mash the avocado with a fork or with a potato masher and make it as chunky or smooth as you like. Add chopped chili/ jalapeño, chopped onion, chopped cilantro, chopped tomato and salt. Stir until well combined., 3. Taste and season with extra salt or lemon juice., 4. Serve with tortilla chips.',
-                'https://www.youtube.com/watch?v=K06J2pFY6yU'
+                'https://www.youtube.com/watch?v=K06J2pFY6yU',
+                'https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg'
         );
 
 --  https://www.budgetbytes.com/hearty-black-bean-quesadillas/
