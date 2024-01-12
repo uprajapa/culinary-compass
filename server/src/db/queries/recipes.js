4a1cf44534fc977601739fc1d5d7ffc6e8faa3ee
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-12 09:46:38 -0500

    add pictures to seeds

--- server/src/db/queries/recipes.js
+++ server/src/db/queries/recipes.js
@@ -31,7 +31,7 @@ const findTopRatedRecipes = async () => {
 };
 const findTopThreeRecipes = async () => {
   try {
-    const query = "SELECT recipes.id AS recipes_id, recipes.chef_name, recipes.recipe_name, recipes.cook_time, recipes.cuisine, recipes.description FROM ratings JOIN recipes ON recipes.id = ratings.recipe_id WHERE rating > 4 GROUP BY recipe_id, recipes.id, recipes.video_link LIMIT 3;";
+    const query = "SELECT recipes.id AS recipes_id, recipes.chef_name, recipes.recipe_name, recipes.cook_time, recipes.cuisine, recipes.description, recipes.photo_link FROM ratings JOIN recipes ON recipes.id = ratings.recipe_id WHERE rating > 4 GROUP BY recipe_id, recipes.id, recipes.video_link LIMIT 3;";
     const result = await db.query(query);
     if (result.rowCount > 0) {
       console.log(result.rows);
