021c4d8a1e08362375966f5b0bfe3cb5449a4300
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-12 09:55:44 -0500

    enter variables to populate carousel pictures from topThreeRecipes array

--- client/src/components/FoodCarousel.jsx
+++ client/src/components/FoodCarousel.jsx
@@ -4,30 +4,26 @@ import "../../public/css/FoodCarousel.css";
 
 const FoodCarousel = (props) => {
   const { topThreeRecipes } = props;
-
+  
   return (
     <div className="carousel">
-      <Carousel>
-          
+      <Carousel> 
             <Carousel.Item controls={true}>
-                <img className="first-pic" src="https://www.closetcooking.com/wp-content/uploads/2008/08/Corn-and-Black-Bean-Quesadillas-500.jpg" />
+                <img className="picture" src={topThreeRecipes[0].photo_link} />
                 <Carousel.Caption>
                   <h3>{topThreeRecipes[0].recipe_name}</h3>
                   <p>{topThreeRecipes[0].description}</p>
                 </Carousel.Caption>
-            </Carousel.Item>
-          
+            </Carousel.Item>  
         <Carousel.Item controls={true}>
-          <img 
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/8I37NtDffNV7AZlDa7uDvvqhovU.jpg"/>
+          <img className="picture" src={topThreeRecipes[1].photo_link}/>
           <Carousel.Caption>
             <h3>{topThreeRecipes[1].recipe_name}</h3>
             <p>{topThreeRecipes[1].description}</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item controls={true}>
-          <img className="first-pic" src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Hearty-Black-Bean-Quesadillas-V3.jpg"/>
+          <img className="picture"src={topThreeRecipes[2].photo_link}/>
           <Carousel.Caption>
             <h3>{topThreeRecipes[2].recipe_name}</h3>
             <p>
