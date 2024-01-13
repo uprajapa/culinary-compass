8726580768afe32984357130310bc4986f88296f
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-13 15:53:08 -0500

    create italianRecipes component

---
+++ client/src/pages/italianRecipes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecipeList from '../components/RecipeList';
+import "../../public/css/recipe.css"
+
+function italianRecipes({ recipes, favorite, handleFavorite }) {
+  console.log(recipes)
+  return (
+    <div className="recipe">
+      <h1 className="title">All Recipes</h1>
+      <RecipeList recipes={recipes} favorite={favorite} handleFavorite={handleFavorite}/>
+    </div>
+  )
+};
+
+export default italianRecipes;
