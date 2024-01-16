ddc8bf75ad278d864ae95b7ed86833b82612dbd2
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-15 22:24:48 -0500

    add li tag

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -7,19 +7,21 @@ const RecipeList = (props) => {
   const { recipes, favorite, handleFavorite } = props;
   
   const recipeListArray = recipes.map((recipe)=>
-    <RecipeListItem 
-    key={recipe.id}
-    id={recipe.id}
-    recipe_name={recipe.recipe_name}
-    chef_name={recipe.chef_name}
-    budget={recipe.budget}
-    time={recipe.cook_time}
-    cuisine={recipe.cuisine_name}
-    description={recipe.description}
-    photo_link={recipe.photo_link}
-    favorite={favorite}
-    handleFavorite={handleFavorite}
-    />
+    <li>
+      <RecipeListItem 
+      key={recipe.id}
+      id={recipe.id}
+      recipe_name={recipe.recipe_name}
+      chef_name={recipe.chef_name}
+      budget={recipe.budget}
+      time={recipe.cook_time}
+      cuisine={recipe.cuisine_name}
+      description={recipe.description}
+      photo_link={recipe.photo_link}
+      favorite={favorite}
+      handleFavorite={handleFavorite}
+      />
+    </li>
   );
   return(
     <ul>
