ed36378823c64988c50f2695fe629245632ca780
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-17 09:17:23 -0500

    add ON DELETE CASCADE

--- server/src/db/schema/05_favorites.sql
+++ server/src/db/schema/05_favorites.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS favorites;
 CREATE TABLE favorites (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
-  recipe_id INTEGER REFERENCES recipes(id) NOT NULL
+  recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE NOT NULL
 );
--- server/src/db/schema/06_ratings.sql
+++ server/src/db/schema/06_ratings.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS ratings CASCADE;
 CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
-  recipe_id INTEGER REFERENCES recipes(id) NOT NULL,
+  recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE NOT NULL,
   rating INTEGER NOT NULL CHECK (rating between 0 and 5)
 );
