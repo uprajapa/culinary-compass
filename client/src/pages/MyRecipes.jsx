2f0cb366a7e5cbf87d7eaa26329efcc1e5527f0b
Fahad Kamal <fahadkamal@hotmail.com>
2024-01-17 08:54:26 -0500

    implement axios delete

--- client/src/pages/MyRecipes.jsx
+++ client/src/pages/MyRecipes.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../../public/css/myrecipes.css"
-function MyRecipes() {
-  const testRecipes = [
-              {name: "Mexican Inspired Healthy One Pot Black Bean"}, 
-              {name: "Indian Style Potato Vegetable Wrap"}, 
-              {name: "Quick and Easy Chicken Curry"}
-            ]
-  const [userRecipes, setUserRecipes] = useState(testRecipes);
+import useRecipes from '../hooks/useRecipes';
+import axios from 'axios';
 
+function MyRecipes() {
   
+  const { recipes, setRecipes } = useRecipes();
+
+  const deleteRecipe = (id) => { 
+   axios.delete(`http://localhost:8080/api/recipes/${id}`)
+      .then((response) => console.log("Deleted recipe with id", id, response))
+      .catch(err => console.error('Error deleting recipe', err))  
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  };
+
   return(
     <div className="stuff">
       <h1 className="title">My Recipes</h1>
@@ -19,11 +24,11 @@ function MyRecipes() {
             <td className="title-data">Edit Recipe </td>
             <td className="title-data">Delete Recipe</td>
           </tr>
-        {userRecipes.map(recipe => 
-          <tr key={recipe.name} className="content-row">
-            <td className="content-data">{recipe.name}</td>
+        {recipes.map(recipe => 
+          <tr key={recipe.id} className="content-row">
+            <td className="content-data">{recipe.recipe_name}</td>
             <td className="content-data"><button>Edit</button></td>
-            <td className="content-data"><button>Delete</button></td>
+            <td className="content-data"><button onClick={()=> deleteRecipe(recipe.id)}>Delete</button></td>
           </tr>
           )}
         </tbody>
@@ -34,4 +39,11 @@ function MyRecipes() {
   };
   
   export default MyRecipes;
+  
+  // const testRecipes = [
+  //             {name: "Mexican Inspired Healthy One Pot Black Bean"}, 
+  //             {name: "Indian Style Potato Vegetable Wrap"}, 
+  //             {name: "Quick and Easy Chicken Curry"}
+  //           ]
+  // const [userRecipes, setUserRecipes] = useState(testRecipes);
   
